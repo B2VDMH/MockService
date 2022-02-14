d6d1ca147be6138030fabe88f2b4f7d50881af95
Buzsu <Bazsi2432@yahoo.com>
2022-02-14 12:59:29 +0100

    created starter batch files

---
+++ src/start_script/batch_start_8083.bat
@@ -0,0 +1,1 @@
+java -Dserver.port=8083 -jar C:\Users\Buzsu\IdeaProjects\MockupService\target\MockupService-0.0.1-SNAPSHOT.war
---
+++ src/start_script/batch_start_8084.bat
@@ -0,0 +1,1 @@
+java -Dserver.port=8084 -jar C:\Users\Buzsu\IdeaProjects\MockupService\target\MockupService-0.0.1-SNAPSHOT.war
