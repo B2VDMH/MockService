95a06479685cf9feecd7132dde2d73c0d6b4b453
Buzsu <Bazsi2432@yahoo.com>
2022-02-14 12:59:07 +0100

    created starter batch files

--- src/main/java/hu/gdf/thesis/MockupService/MockupServiceApplication.java
+++ src/main/java/hu/gdf/thesis/MockupService/MockupServiceApplication.java
@@ -3,9 +3,6 @@ package hu.gdf.thesis.MockupService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-import java.io.IOException;
-
 @SpringBootApplication
 public class MockupServiceApplication {
 
---
+++ src/start_script/batch_start.cmd
@@ -0,0 +1,1 @@
+java -Dserver.port=8080 -spring.mvc.servlet.load-on-startup=1 -jar C:\Users\Buzsu\IdeaProjects\MockupService\target\MockupService-0.0.1-SNAPSHOT.jar
